d4c621bb5ffb34bb412c45eea428b3fea230a6b5
franciscomeneses13 <franciscomeneses13@gmail.com>
2019-06-01 14:48:10 -0300

    Ejercicio 1

---
+++ Clase01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,16 @@
+var nombre = prompt('ingresa tu nombre')
+console.log('hola bienvenido: ' + nombre)
+
+var pi = 3.14
+var numeroASumar = prompt('ingresa un numero')
+numeroASumar = parseFloat(numeroASumar)
+console.log(pi + numeroASumar)
+
+var numeroBase = 4
+var numeroARestar = prompt('inserta un numero')
+numeroARestar = parseFloat(numeroARestar)
+console.log(numeroBase - numeroARestar)
+
+var primerNumero = prompt ('este es el primer numero')
+var segundoNumero = prompt ('ahora es el segundo numero')
+primerNumero = parseFloat(primerNumero)
