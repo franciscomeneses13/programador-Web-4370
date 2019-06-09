cede65b85beb384b9f5b3f7d04af9c6425ddb56c
franciscomeneses13 <franciscomeneses13@gmail.com>
2019-06-09 16:16:19 -0300

    desafio 02

--- Clase 02/Alumno/Desafío/js/index.js
+++ Clase 02/Alumno/Desafío/js/index.js
@@ -18,7 +18,9 @@
 //     console.log('Sr. usted es menor de edad no puede ingresar')
 // } else if (prefijo = 'Sra' && edad < 18) {
 //     console.log('Sra. usted es menor de edad no puede ingresar')
-// } 
+// } else if (prefijo = 'Sx' && edad < 18) {
+//     console.log('Sx. usted es menor de edad, pero por no ser cisgenero puede ingresar')
+// }
 
 var genero = prompt('ingrese su genero: ')
 var edad = prompt('ingrese su edad: ')
@@ -46,17 +48,16 @@ console.log(prefijo)
 if (edad >= 18) {
     mensaje = 'usted es mayor de edad puede ingresar'
 } else {
-    mensaje = 'usted es mejor de edad y no puede ingresar'
+    mensaje = 'usted es menor de edad y no puede ingresar'
 }
 console.log(mensaje)
 console.log(prefijo + ' ' + mensaje)
 
 
-if (genero === 'male') {
-    console.log ('es male')
-} else if (genero === 'female'){
-    console.log ('es female')
-} else {
-    console.log('sx.')
-}
- 
+// if (genero === 'male') {
+//     console.log('es male')
+// } else if (genero === 'female') {
+//     console.log('es female')
+// } else {
+//     console.log('sx.')
+// }
